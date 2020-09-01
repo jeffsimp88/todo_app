c76843fade872c7d31c566118ce2829762e60d05
Jeffrey Simpson <jdsimpson806@gmail.com>
2020-09-01 15:13:28 -0400

    Created Component Files

---
+++ src/components/todoList/TodoList.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import TodoItem from '../todoItems/TodoItem'
+
+ function TodoList (props) {
+ 
+    return (
+      <section className="main">
+        <ul className="todo-list">
+          {props.todos.map((todo) => (
+            <TodoItem key={todo.id} title={todo.title} completed={todo.completed} id={todo.id} />
+          ))}
+        </ul>
+      </section>
+    )
+}
+
+const mapStateToProps = (state) => {
+  return{
+    todos: state.todos
+  }
+}
+export default connect(mapStateToProps)(TodoList)
