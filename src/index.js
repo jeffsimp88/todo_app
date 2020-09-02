a66fb274548fb2a0900ba333ad10f6807aba17fa
Jeffrey Simpson <jdsimpson806@gmail.com>
2020-09-02 06:46:00 -0400

    Updated Store info

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {Provider} from 'react-redux'
-import {store} from './redux/store'
+import store from './redux/store'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />    
+  </Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
