0a52f0116b6538b7a660828f1b56b77614d15dd2
Jeffrey Simpson <69972924+jeffsimp88@users.noreply.github.com>
2021-04-27 14:55:00 -0400

    Added state to show todos left to complete

--- src/App.js
+++ src/App.js
@@ -1,48 +1,55 @@
-import React, {useState} from "react"
-import TodoList from './components/todoList/TodoList'
-import {addTodo, clearCompletedTodos} from './redux/index'
-import { connect } from "react-redux"
+import React, { useState } from "react";
+import TodoList from "./components/todoList/TodoList";
+import { addTodo, clearCompletedTodos } from "./redux/index";
+import { connect, useSelector } from "react-redux";
 
-function App (props){
-  const [input, setInput] = useState("")
- 
-  function handleKeyPress(event){
-      if(event.key === 'Enter'){
-       props.addTodo(input)
-      setInput("")
-      }    
+function App(props) {
+  const [input, setInput] = useState("");
+  const todosList = useSelector((state) => state.todos);
+  const todosLeft = todosList.filter((x) => x.completed === false);
+
+  function handleKeyPress(event) {
+    if (event.key === "Enter") {
+      props.addTodo(input);
+      setInput("");
+    }
   }
-    return (
-      <section className="todoapp">
-        <header className="header">
-          <h1>todos</h1>
-          <input 
-            onKeyPress={handleKeyPress}
-            className="new-todo" 
-            placeholder="What needs to be done?" 
-            value={input} 
-            onChange={e => setInput(e.target.value)} 
-            autoFocus 
-          />
-        </header>
-        <TodoList/>
-        <footer className="footer">
-          <span className="todo-count">
-            <strong>0</strong> item(s) left
-          </span>
-          <button onClick={() => props.clearCompletedTodos()} className="clear-completed">Clear completed</button>
-        </footer>
-      </section>
-    )
+  return (
+    <section className="todoapp">
+      <header className="header">
+        <h1>todos</h1>
+        <input
+          onKeyPress={handleKeyPress}
+          className="new-todo"
+          placeholder="What needs to be done?"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          autoFocus
+        />
+      </header>
+      <TodoList />
+      <footer className="footer">
+        <span className="todo-count">
+          <strong>{todosLeft.length}</strong> item(s) left
+        </span>
+        <button
+          onClick={() => props.clearCompletedTodos()}
+          className="clear-completed"
+        >
+          Clear completed
+        </button>
+      </footer>
+    </section>
+  );
 }
 
-const mapStateToProps = (state) =>({
-  todos: state.todos
-})
+const mapStateToProps = (state) => ({
+  todos: state.todos,
+});
 
-const mapDispatchToProps = (dispatch) =>({
+const mapDispatchToProps = (dispatch) => ({
   addTodo: (text) => dispatch(addTodo(text)),
-  clearCompletedTodos: () => dispatch(clearCompletedTodos())
-})
+  clearCompletedTodos: () => dispatch(clearCompletedTodos()),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
