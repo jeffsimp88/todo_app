2cdbb4f9f723e376197a7d9c73e4fdc8045a64dd
Jeffrey Simpson <69972924+jeffsimp88@users.noreply.github.com>
2021-04-27 15:42:11 -0400

    Updates

---
+++ precache-manifest.290b41aeb9c9162c26e95e89617098ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7b5f8fbf888ffc60f9de78a35816b4d",
+    "url": "/todo_app/index.html"
+  },
+  {
+    "revision": "4a1a9ecea71029368b43",
+    "url": "/todo_app/static/css/main.5f80b972.chunk.css"
+  },
+  {
+    "revision": "2b092ce673a5d3ba08fe",
+    "url": "/todo_app/static/js/2.05adb1fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo_app/static/js/2.05adb1fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a1a9ecea71029368b43",
+    "url": "/todo_app/static/js/main.0c1b9e04.chunk.js"
+  },
+  {
+    "revision": "bfa9aee4d864d81eb28d",
+    "url": "/todo_app/static/js/runtime-main.4ab88a25.js"
+  }
+]);
